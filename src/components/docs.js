26d3724856ef5e49b25f977b50073e396b2c4fe2
Adam Tak <adam.tak2112@gmail.com>
2023-01-25 14:24:03 -0700

    first commit

---
+++ src/components/docs.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function Docs(){
+    return (
+        <div class="docs-main">
+            <h1>docs</h1>
+            <button className="new-doc-button">
+                Add New Document
+            </button>
+        </div>
+    );
+}
