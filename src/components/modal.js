cc297eef7634cb57e0cfbb0548cbb778fc21f88f
Adam Tak <adam.tak2112@gmail.com>
2023-01-26 09:03:35 -0700

    basic box implementation

--- src/components/modal.js
+++ src/components/modal.js
@@ -15,3 +15,25 @@ const style = {
     boxShadow: 24,
     p: 4,
 };
+
+export default function Modal() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    
+                </Box>
+
+            </Modal>
+        </div>
+    );
+}
