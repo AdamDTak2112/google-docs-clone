5dc5fa75829c48217d3b0f71cf1b3792cc228423
Adam Tak <adam.tak2112@gmail.com>
2023-02-14 11:12:35 -0700

    add button and input to new doc modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -31,12 +31,17 @@ export default function ModalComponent({
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
+                    <input 
+                        placeholder="Add a Title"
+                        className="add-input"
+                    />
+                    <div class="title-button-container">
+                        <Button
+                            className="new-doc-button"
+                        >
+                            OK
+                        </Button>
+                    </div>
                     
                 </Box>
 
