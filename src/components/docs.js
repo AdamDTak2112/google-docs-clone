f6b2275cb237ba1d71e56e33ff07c77f8729dc1a
Adam Tak <adam.tak2112@gmail.com>
2023-02-14 10:38:54 -0700

    added mui, integrate modal component

--- src/components/docs.js
+++ src/components/docs.js
@@ -2,9 +2,11 @@ import React from "react";
 
 
 export default function Docs(){
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
     return (
         <div class="docs-main">
-            <h1>docs</h1>
+            <h1>Docs Clone</h1>
             <button className="new-doc-button">
                 Add New Document
             </button>
--- src/components/modal.js
+++ src/components/modal.js
@@ -30,8 +30,11 @@ export default function Modal() {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <Typography>
-                        
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Text in a modal
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
                     </Typography>
                     
                 </Box>
