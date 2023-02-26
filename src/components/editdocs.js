ef7d21b72847a5e8d026f8266177162616e62e2a
Adam Tak <adam.tak2112@gmail.com>
2023-02-25 18:06:07 -0700

    adding state for ReactQuill

--- src/components/editdocs.js
+++ src/components/editdocs.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { useParams } from 'react-router-dom';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export default function EditDocs(){
     let params= useParams();
     console.log(params);
+    const [docsDesc, setDocsDesc] = useState('');
+    const getQuillData = () => {
+        
+    }
     return (
-        <div>EditDocs</div>
+        <div>
+            <h1>EditDocs Page</h1>
+
+            <ReactQuill />
+        </div>
     )
 }
