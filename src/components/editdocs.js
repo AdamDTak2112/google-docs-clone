cd47c3240fbdf1cba6c13280b57feb9b2897a809
Adam Tak <adam.tak2112@gmail.com>
2023-02-25 18:00:57 -0700

    importing params to editdocs view

--- src/components/editdocs.js
+++ src/components/editdocs.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useParams } from 'react-router-dom';
 
 export default function EditDocs(){
+    let params= useParams();
+    console.log(params);
     return (
         <div>EditDocs</div>
     )
